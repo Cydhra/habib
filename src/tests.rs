419e5a8cc52341e19aed03546f1a6368a1b81953
Johannes Hengstler <ubezl@student.kit.edu>
2024-04-30 01:25:25 +0200

    added test for multiple collisions

--- src/tests.rs
+++ src/tests.rs
@@ -403,3 +403,64 @@ fn test_collisions_replacement() {
     assert_eq!(map.get_right(&31), Some(&5));
     assert_eq!(map.get_left(&5), Some(&31));
 }
+
+#[test]
+fn test_multi_collision() {
+    // test whether a lot of collisions are resolved correctly
+    let mut map = BiMap::with_hashers(DEFAULT_CAPACITY, IdentityHasher::default(), IdentityHasher::default());
+
+    for i in 0..10 {
+        map.insert(i * DEFAULT_CAPACITY + 1, i + 1);
+
+        for j in 0..=i {
+            // verify the overflow slots are actually used (otherwise the test is broken)
+            assert_ne!(map.left_index[1 + j], EMPTY_SLOT);
+        }
+
+        // verify the next slot after all overflow slots is empty
+        assert_eq!(map.left_index[1 + i + 1], EMPTY_SLOT);
+
+        for j in 0..=i {
+            assert_eq!(map.get_right(&(j * DEFAULT_CAPACITY + 1)), Some(&(j + 1)));
+            assert_eq!(map.get_left(&(j + 1)), Some(&(j * DEFAULT_CAPACITY + 1)));
+        }
+    }
+
+    // test whether a lot of collisions are resolved correctly,
+    // some of which wrap around the end of the array
+    let mut map = BiMap::with_hashers(DEFAULT_CAPACITY, IdentityHasher::default(), IdentityHasher::default());
+
+    for i in 0..10 {
+        map.insert(i * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2), i + 1);
+
+        for j in 0..=i {
+            assert_eq!(map.get_right(&(j * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2))), Some(&(j + 1)));
+            assert_eq!(map.get_left(&(j + 1)), Some(&(j * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2))));
+        }
+    }
+
+    // test whether a lot of collisions are resolved correctly,
+    // some of which wrap around the end of the array, with some deletions
+
+    map.remove_left(&(DEFAULT_CAPACITY - 2));
+
+    assert_eq!(map.get_right(&(DEFAULT_CAPACITY - 2)), None);
+    assert_eq!(map.get_left(&1), None);
+
+    for j in 1..10 {
+        assert_eq!(map.get_right(&(j * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2))), Some(&(j + 1)));
+        assert_eq!(map.get_left(&(j + 1)), Some(&(j * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2))));
+    }
+
+    map.remove_right(&4);
+
+    for j in 1..10 {
+        if j != 3 {
+            assert_eq!(map.get_right(&(j * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2))), Some(&(j + 1)));
+            assert_eq!(map.get_left(&(j + 1)), Some(&(j * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2))));
+        } else {
+            assert_eq!(map.get_right(&(j * DEFAULT_CAPACITY + (DEFAULT_CAPACITY - 2))), None);
+            assert_eq!(map.get_left(&(j + 1)), None);
+        }
+    }
+}
