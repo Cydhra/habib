5261021c579fc65a325e536d325394c7f93baf3e
Johannes Hengstler <ubezl@student.kit.edu>
2025-02-15 21:28:48 +0100

    cargo fmt

--- benches/get.rs
+++ benches/get.rs
@@ -1,10 +1,10 @@
 mod common;
 
+use crate::common::*;
+use bijective_map::BiMap;
 use criterion::*;
 use permutation_iterator::Permutor;
-use rand::{RngCore, thread_rng};
-use bijective_map::BiMap;
-use crate::common::*;
+use rand::{thread_rng, RngCore};
 
 fn bench_get(c: &mut Criterion) {
     let mut rng = thread_rng();
@@ -15,8 +15,10 @@ fn bench_get(c: &mut Criterion) {
     for load_factor in [0.5, 0.75, 0.8, 0.9] {
         for length in SIZES {
             let mut map = BiMap::with_capacity(length);
-            let mut permutor_left = Permutor::new_with_u64_key(u64::MAX, rng.next_u64()).into_iter();
-            let mut permutor_right = Permutor::new_with_u64_key(u64::MAX, rng.next_u64()).into_iter();
+            let mut permutor_left =
+                Permutor::new_with_u64_key(u64::MAX, rng.next_u64()).into_iter();
+            let mut permutor_right =
+                Permutor::new_with_u64_key(u64::MAX, rng.next_u64()).into_iter();
 
             let entire_length = (length as f64 * (load_factor / 0.9)) as usize;
             for _ in 0..entire_length {
@@ -25,9 +27,17 @@ fn bench_get(c: &mut Criterion) {
                 map.insert(left, right);
             }
 
-            group.bench_with_input(BenchmarkId::new(format!("get_{}", load_factor), length), &length, |b, _| {
-                b.iter_batched(|| rng.next_u64() % entire_length as u64, |key| map.get_left(&key), BatchSize::SmallInput);
-            });
+            group.bench_with_input(
+                BenchmarkId::new(format!("get_{}", load_factor), length),
+                &length,
+                |b, _| {
+                    b.iter_batched(
+                        || rng.next_u64() % entire_length as u64,
+                        |key| map.get_left(&key),
+                        BatchSize::SmallInput,
+                    );
+                },
+            );
         }
     }
 
